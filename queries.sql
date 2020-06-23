ad1b3d7db6a804c113c98bf69f487357963b40a4
Marissa <marissacooterl@gmail.com>
2020-06-23 16:28:25 -0500

    still working on queries, moving on to building scheme-model

--- queries.sql
+++ queries.sql
@@ -1,9 +1,12 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+    -- A: SELECT * FROM "Product" JOIN "Category" ON "Product"."CategoryId" = "Category"."Id"
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+    -- A:
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+    -- A:  
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
