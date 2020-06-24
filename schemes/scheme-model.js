d7a4f60ad6fce2cc49510621a556dbd7a54a7974
Marissa <marissacooterl@gmail.com>
2020-06-24 14:21:55 -0500

    debugging delete, put, post by id steps

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -5,6 +5,30 @@ function find() {
     return db("schemes")
 }
 
+// GET :id
+function findById(id) {
+    return db('schemes').where({id})
+}
+
+// GET :id steps
+function findSteps(id) {
+    return db('steps').where({id})
+}
+
+// POST
+function add(schemeData) {
+    return db('schemes').insert(schemeData)
+}
+
+// DELETE
+function remove(id) {
+    return db('schemes').where({id}).remove()
+}
+
 module.exports = {
     find,
+    findById,
+    findSteps,
+    add,
+    remove
 }
